2171046824e4a5b8ef37d9dc16e239935e8959a7
Maxim Vintsyun <maxes2000@yandex.ru>
2023-12-13 18:41:37 +0500

    fix: небольшие исправления

--- controllers/movies.js
+++ controllers/movies.js
@@ -51,7 +51,7 @@ module.exports.createMovie = (req, res, next) => {
 };
 
 module.exports.deleteMovie = (req, res, next) => {
-  Movie.findById(req.params._id)
+  Movie.findById(req.params.movieId)
     .orFail(new NotFoundError('Фильм не найден'))
     .then((movie) => {
       if (movie.owner.toString() !== req.user._id) {

4a02df8fdc540597dd5b4872106781ace785c264
Maxim Vintsyun <maxes2000@yandex.ru>
2023-12-13 18:38:56 +0500

    fix: небольшие исправления

--- routes/movies.js
+++ routes/movies.js
@@ -19,9 +19,9 @@ router.post('/', celebrate({
     nameEN: Joi.string().required(),
   }),
 }), createMovie);
-router.delete('/:_id', celebrate({
+router.delete('/:movieId', celebrate({
   params: Joi.object().keys({
-    _id: Joi.string().length(24).hex().required(),
+    movieId: Joi.number().required(),
   }),
 }), deleteMovie);
 
